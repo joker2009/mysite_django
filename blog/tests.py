35efb43be794d88e4f7b81477ef8c37e113bfc97
joker2009 <304640509@qq.com>
2017-11-22 19:53:36 +0800

    first commit

---
+++ blog/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+# Create your tests here.
+# from django.contrib.auth.models import User
+# from blog.models improt Post
+#
+# user = User.objects.get(username='admin')
+# post = Post.objects.create(title='One more post')
