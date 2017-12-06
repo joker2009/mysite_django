f34f58221da30ca31d67cb889a5a479ec28dd2f4
joker2009 <304640509@qq.com>
2017-12-06 18:57:25 +0800

    email

--- blog/views.py
+++ blog/views.py
@@ -56,16 +56,22 @@ def post_share(request, post_id):
 
     post = get_object_or_404(Post, id=post_id, status='published')
     sent = False
+    print('0')
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
+        print('1')
         if form.is_valid():
             cd = form.cleaned_data
+            print('2')
             post_url = request.build_absolute_uri(post.get_absolute_url())
             subject = '{}({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
             message = 'Read "{}" at {}\n\n{}\'s comments:{}'.format(post.title, post_url, cd['name'], cd['comments'])
-            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            print(3)
+            send_mail(subject, message, '304640509@qq.com', [cd['to']])
             sent = True
+            print(4)
     else:
         form = EmailPostForm()
+    print(5)
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
 
